c82b4d6869df875b8d5edeaa887b86ccddcd5d4c
Alex Boster <boster@tilthouse.org>
2011-08-25 17:06:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deals::Application.config.secret_token = '0d2d8acfcda2a03f84e264dea1fbd5418facf8304cdbed2a0b247d5b21b380aea8d69578e1b74b5603895bfec4f1c1c6ad8660fed7caaefee3a5692090d4df56'
